e76d3b50ab210cb24edab6582a3584fecc7d1eb3
Bruno da Costa Paraiso <brunoparaisoweb@gmail.com>
2026-01-22 09:59:40 -0300

    excluindo arquivos desnecessarios

--- brasileirao_last5/check_names.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from sofascore_scraper_ligue1 import scrape_sofascore_last5
-
-jogos = scrape_sofascore_last5(1659, 'Strasbourg')
-print('Nomes dos times nos jogos do Strasbourg:')
-for j in jogos:
-    print(f"  Mandante: '{j['mandante']}', Visitante: '{j['visitante']}'")
